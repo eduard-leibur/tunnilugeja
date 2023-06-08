77b3e96a8a12dfccb941d3948914a1c77665d138
eduard-leibur <99919791+eduard-leibur@users.noreply.github.com>
2023-06-08 13:15:09 +0300

    08.06 algus

---
+++ main.py
@@ -0,0 +1,10 @@
+print("1 - sisesta töötunde\n"
+      "2 - kuva töötunnid\n"
+      "3 - lõpeta töö")
+sisend = input("Vali tegevus: ")
+if sisend == "1":
+
+elif sisend == "2":
+
+elif sisend == "3" or "lõpp":
+      quit(1)
