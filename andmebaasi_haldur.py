c0a275785e7d9c5c4013c73b65b471ac869b8eca
eduard-leibur <99919791+eduard-leibur@users.noreply.github.com>
2023-06-09 12:43:25 +0300

    andmebaas töötab: saab sisestada ja kuvada

--- andmebaasi_haldur.py
+++ andmebaasi_haldur.py
@@ -7,13 +7,23 @@ class Andmehaldur:
         self.kursor = self.ühendus.cursor()
 
     def loo_tabel(self):
-        self.kursor.execute("CREATE TABLE töö(kuupäev, aeg, selgitus)")
+        self.kursor.execute("""CREATE TABLE töö(
+                            kuupäev TEXT NOT NULL,
+                            aeg INTEGER NOT NULL,
+                            selgitus TEXT,
+                            makstud INTEGER NOT NULL
+                            )""")
 
     def lisa_rida(self, kuupäev, aeg: int, selgitus):
-        self.kursor.execute("INSERT INTO töö(kuupäev, aeg, selgitus) VALUES(?, ?, ?)", (kuupäev, aeg, selgitus))
+        self.kursor.execute("INSERT INTO töö(kuupäev, aeg, selgitus, makstud) VALUES(?, ?, ?, ?)",
+                            (kuupäev, aeg, selgitus, False))
 
     def tühjenda_tabel(self):
         self.kursor.execute("DELETE FROM töö")
+
+    def kõigi_valik(self):
+        valik = self.kursor.execute("SELECT rowid, kuupäev, makstud, aeg, selgitus FROM töö")
+        return valik
 
     def väljumine(self):
         self.ühendus.commit()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("1 - sisesta töötunde\n"
       "2 - kuva töötunnid\n"
       "3 - lõpeta töö")
 
-while True:
+while __name__ == "__main__":
     sisend = input("Vali tegevus: ")
     if sisend == "1":
 
@@ -13,7 +13,7 @@ while True:
         minuteid = int(input("Mitu minutit? "))
         aeg = minuteid + tunde * 60
 
-        kuupäev = input("Kuupäev (või \"täna\"): ")
+        kuupäev = input("Kuupäev (\"täna\" või \"dd.mm\"): ")
         if kuupäev.lower() == "täna":
             praegune_aeg = datetime.datetime.now()
             päev = praegune_aeg.strftime("%d")
@@ -25,10 +25,29 @@ while True:
         andmebaas = Andmehaldur("andmebaas.db")
         andmebaas.lisa_rida(kuupäev, aeg, selgitus)
         andmebaas.väljumine()
+        print("Töötunnid salvestatud!\n")
 
     elif sisend == "2":
-        print("Looda vaid.\n")
-    elif sisend == "3" or "lõpp":
-        break
+        andmebaas = Andmehaldur("andmebaas.db")
+        for andmerida in andmebaas.kõigi_valik():
+            nr = andmerida[0]
+            kuupäev = andmerida[1]
+            aeg = andmerida[3]
+            selgitus = andmerida[4]
+            if andmerida[2] == 0:
+                makstud = "-"
+            else:
+                makstud = "+"
+            if aeg >= 60:
+                tunde = aeg // 60
+                minuteid = aeg % 60
+                aeg = f"{tunde}h {minuteid}min"
+            else:
+                aeg = str(aeg) + "min"
+
+            print(f"nr.{nr}  {makstud}  {kuupäev}  {aeg}  {selgitus}")
+
+    elif sisend == "3" or sisend == "lõpp" or sisend == "0":
+        quit(1)
     else:
-        print("Ei saanud aru.\n")
+        print("Ei saanud aru.")
